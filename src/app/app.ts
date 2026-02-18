ff737b871e7bfdea3e6965fdd554a77d5c97eb24
ziad <shalabyziad94@gmail.com>
2026-02-18 18:43:24 +0200

    'update'

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Footer } from './../../projects/ngx-zs-component/src/lib/ngx-zs-component/footer/footer';
 import { ScrollToTop } from './../../projects/ngx-zs-component/src/lib/ngx-zs-component/scroll-to-top/scroll-to-top';
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Test } from './test/test';
 import { ThemeToggle, themeTypes } from "../../projects/ngx-zs-component/src/lib/ngx-zs-component/theme-toggle/theme-toggle";
 import { Button } from '../../projects/ngx-zs-component/src/lib/ngx-zs-component/FormCompFolder/button/button';
 import { Alert } from '../../projects/ngx-zs-component/src/lib/ngx-zs-component/AlertFolder/alert/alert';
+import { AlertService } from '../../projects/ngx-zs-component/src/lib/ngx-zs-component/AlertFolder/alert-service/alert-service';
 
 @Component({
   selector: 'app-root',
@@ -24,4 +25,14 @@ import { Alert } from '../../projects/ngx-zs-component/src/lib/ngx-zs-component/
 export class App {
   protected readonly title = signal('ngx-zs-component-project');
   readonly setTheme = signal<themeTypes | null>(null)
+
+  readonly alert = inject(AlertService);
+
+  constructor() {
+    this.alert.addAlert({
+      type: 'success',
+      message: 'Loggedin Successfully, Welcome Again',
+      autoClose: false
+    })
+  }
 }
